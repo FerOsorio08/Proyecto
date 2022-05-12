6a43aa3ce1ecd8ea06dd37c4d29085870cdbb9e6
Emo Salazar <emiliasalazar67@gmail.com>
2022-05-12 12:44:14 -0500

    sin error creo

--- paint.py
+++ paint.py
@@ -41,10 +41,27 @@ def circle(start, end):
         turtle.color(colors[i%6])
 def rectangle(start, end):
     "Draw rectangle from start to end."
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+
+    for count in range(2):
+        forward(end.x - start.x)
+        left(90)
+        forward(end.y - start.y)
+        left(90)
+    end_fill()
 def triangle(start, end):
     "Draw triangle from start to end."
-    pass  # TODO
+    up()
+    goto(start.x, start.y)
+    down()
+    begin_fill()
+    for count in range(3):
+        forward(end.x - start.x)
+        left(120)
+    end_fill()
 def tap(x, y):
     "Store starting point or draw shape."
     start = state['start']

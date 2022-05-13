5a9c1a95ac6387c24a1ec7bba58a10857a62474c
Fernanda Cortes <fercortlo@icloud.com>
2022-05-13 09:15:53 -0500

    Sets limits and moves snake

--- snake.py
+++ snake.py
@@ -1,6 +1,6 @@
 """Snake, classic arcade game.
 
-Exercises
+Exercise
 
 1. How do you make the snake faster or slower?
 2. How can you make the snake go around the edges?
@@ -15,9 +15,6 @@ from freegames import square, vector
 #from random import randit
 from random import choice
 
-food = vector(0, 0)
-snake = [vector(10, 0)]
-aim = vector(0, -10)
 colors = ['blue','black','yellow','pink','green']
 #colorcito = randint(0, 4)
 #colorcito2 = randint(0, 4)
@@ -25,50 +22,65 @@ colores = [0,1,2,3,4]
 selection = choice(colores)
 selection2 = choice(colores)
 
+snake = [[10, 0]]
+aim = [0, -10]
+food = [20, 0]
+
 def change(x, y):
-    "Change snake direction."
-    aim.x = x
-    aim.y = y
+  aim[0] = x
+  aim[1] = y
+  
+def draw_square(x, y, size, name):
+    up()
+    goto(x, y)
+    down()
+    color(name)
+    begin_fill()
 
+    for count in range(4):
+      forward(size)
+      left(90)
+
+    end_fill()
+  
 def inside(head):
-    "Return True if head inside boundaries."
-    return -200 < head.x < 190 and -200 < head.y < 190
-
+    return -200 < head[0] < 190 and -200 < head[1] < 190
+    
 def move():
-    "Move snake forward one segment."
-    head = snake[-1].copy()
-    head.move(aim)
+  head = [snake[-1][0] + aim[0], snake[-1][1] + aim[1]]
 
-    if not inside(head) or head in snake:
-        square(head.x, head.y, 9, 'red')
-        update()
-        return
-
-    snake.append(head)
-
-    if head == food:
-        print('Snake:', len(snake))
-        food.x = randrange(-15, 15) * 10
-        food.y = randrange(-15, 15) * 10
-    else:
-        snake.pop(0)
-
-    clear()
-
-    for body in snake:
-        square(body.x, body.y, 9, colors[selection])
-
-    square(food.x, food.y, 9, colors[selection2])
+  if not inside(head) or head in snake:
+    draw_square(head[0], head[1], 9, 'red')
     update()
-    ontimer(move, 100)
+    return
 
-setup(420, 420, 370, 0)
+  snake.append(head)
+  
+  if head == food:
+    food[0] = randrange(-15, 15) * 10
+    food[1] = randrange(-15, 15) * 10
+  else:
+    snake.pop(0)
+    
+  clear()
+  
+  for body in snake:
+    draw_square(body[0], body[1], 9, colors[selection])
+    
+  draw_square(food[0], food[1], 9, colors[selection2])
+  update()
+  Screen().ontimer(move, 100)
+
+Screen().setup(420, 420, 370, 0)
 hideturtle()
-tracer(False)
-listen()
-onkey(lambda: change(10, 0), 'Right')
-onkey(lambda: change(-10, 0), 'Left')
-onkey(lambda: change(0, 10), 'Up')
-onkey(lambda: change(0, -10), 'Down')
+Screen().tracer(0, 0)
+
+Screen().listen()
+Screen().onkey(lambda: change(10, 0), 'Right')
+Screen().onkey(lambda: change(-10, 0), 'Left')
+Screen().onkey(lambda: change(0, 10), 'Up')
+Screen().onkey(lambda: change(0, -10), 'Down')
+
 move()
+
 done()
